214254a2b34fd53754b5d7075d256844b015a4ed
FayB <gfay0105@163.com>
2020-12-28 20:00:55 +0800

    Create 8test.py

---
+++ Python/BOOK1/8test.py
@@ -0,0 +1,18 @@
+#8-1
+def display_message():
+    print("Study how to define a function and use it!")
+
+display_message()
+
+#8-2
+def favorite_movie(title):
+    print("One of my favorite movie is "+ title + ".")
+
+favorite_movie("《Call me by your name》")
+
+#8.2.1
+def describe_pet(animal_type,pet_name):
+    print ("I have a "+ animal_type + ",and I call it " + pet_name +"!")
+
+describe_pet("dog","coder")
+describe_pet("cat","milk")
