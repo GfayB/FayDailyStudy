1189b5d840b76eaa798cd63896e7efa4f8c8b012
FayB <gfay0105@163.com>
2021-01-08 17:52:36 +0800

    Update 8test.py

--- Python/BOOK1/8test.py
+++ Python/BOOK1/8test.py
@@ -42,3 +42,18 @@ def get_formatted_name (first_name,last_name):
     return full_name.title()
 musician = get_formatted_name('jimi','hendrix')
 print(musician)
+
+#将实参变为可选的
+#给选填的middle_name设置一个默认值：空字符串
+def get_formatted_name(first_name,last_name,middle_name = ''):
+    #使用if语句判定后续是否给middle_name赋值
+    if middle_name:
+        full_name = first_name +' '+ middle_name +' '+ last_name
+    else:
+        full_name = first_name +' '+ last_name
+    return full_name.title()
+
+musician1 =get_formatted_name('jimi','hendrix')
+musician2 =get_formatted_name('john','kooker','lee')
+print (musician1)
+print (musician2
