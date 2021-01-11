29bf01aa819ae1fdef0837498bc6b65b0d3d73bf
FayB <gfay0105@163.com>
2021-01-11 16:12:05 +0800

    Update 8test.py

--- Python/BOOK1/8test.py
+++ Python/BOOK1/8test.py
@@ -57,3 +57,22 @@ musician1 =get_formatted_name('jimi','hendrix')
 musician2 =get_formatted_name('john','kooker','lee')
 print (musician1)
 print (musician2
+
+       
+       #8.3.4 函数和while的结合使用
+def get_formatted_name(first_name,last_name):
+    full_name = first_name + " " +last_name
+    return full_name.title()
+
+while True:
+    print ("\nPlease tell me your name:")
+    print ("(Enter 'q' if you want to quit!)")
+
+    f_name = input("Please insert your first name:")
+    if f_name == 'q':
+        break
+    l_name = input ("Please insert your last name:")
+    if l_name =='q':
+        break
+    formatted_name = get_formatted_name(f_name,l_name)
+    print("\nHello,"+formatted_name + "!")
